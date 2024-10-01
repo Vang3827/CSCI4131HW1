20c43f27b996a388d1486c7b80cd51cda4bb7f02
unknown <vang3827@umn.edu>
2024-10-01 14:33:13 -0500

    working dynamic html

--- server.py
+++ server.py
@@ -2,26 +2,29 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import re
 
 listings = [
-    {"vehicle 1":"Dodge Challenger","url":"https://images.unsplash.com/photo-1632686341369-8a7991237930?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D","description":"Longer text for descrption", "category":"coupe", "numeric ID": "1", "Date": "08/20/2024",
+    {"vehicle 1":"Dodge Challenger","url":"static/html/listing1.html","description":"Longer text for descrption", "category":"coupe", "numeric ID": 1, "Date": "08/20/2024",
      "bids":[
          {"bidder 1":"Carl k", "bid amount":"4,500", "comment":"I would love this car."},{"bidder 2":"Sam Samson", "bid amount":"5,500", "comment":"I just want to use money."}
          ]
     },
-    {"vehicle 2":"Ford Mustang","url":"https://images.unsplash.com/photo-1625231334168-35067f8853ed?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D","description":"Longer text for descrption", "category":"coupe", "numeric ID": "2", "Date": "09/15/2024",
+    {"vehicle 2":"Ford Mustang","url":"static/html/listing2.html","description":"Longer text for descrption", "category":"coupe", "numeric ID": 2, "Date": "09/15/2024",
      "bids":[
          {"bidder 1":"Luke Lukenson", "bid amount":"2,500", "comment":"Dailer driver right here."},{"bidder 2":"Peter Porker", "bid amount":"6,500", "comment":""}
          ]},
-    {"vehicle 3":"Toyota Tundra","url":"https://images.unsplash.com/photo-1621993202323-f438eec934ff?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D","description":"Longer text for descrption", "category":"truck", "numeric ID": "3", "Date": "09/10/2024",
+    {"vehicle 3":"Toyota Tundra","url":"https://images.unsplash.com/photo-1621993202323-f438eec934ff?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D","description":"Longer text for descrption", "category":"truck", "numeric ID": 3, "Date": "09/10/2024",
      "bids":[
          {"bidder 1":"Adam Apple", "bid amount":"6,500", "comment":"Work truck."},{"bidder 2":"Ben Benji", "bid amount":"6,500", "comment":"New truck for me."},{"bidder 3":"Cedar Cider", "bid amount":"9,500", "comment":"The Cedar Mobile."}
          ]},
-    {"vehicle 4":"Subaru Forester","url":"https://images.unsplash.com/photo-1710171940308-8f9670ecfeda?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D","description":"Longer text for descrption", "category":"suv", "numeric ID": "3", "Date": "09/10/2024",
+    {"vehicle 4":"Subaru Forester","url":"https://images.unsplash.com/photo-1710171940308-8f9670ecfeda?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D","description":"Longer text for descrption", "category":"suv", "numeric ID": 4, "Date": "09/10/2024",
      "bids":[
          {"bidder 1":"Mike Micheal", "bid amount":"3,500", "comment":""},{"bidder 2":"Justin Time", "bid amount":"4,500", "comment":"Family vehicle."}
          ]}
 ]
 
 # PUT YOUR GLOBAL VARIABLES AND HELPER FUNCTIONS HERE.
+# path = ""
+# listing_id =""
+
 
 
 def escape_html(str):
@@ -68,7 +71,27 @@ def parse_query_parameters(response):
 
 
 def render_listing(listing):
-    pass
+    print("in render listing")
+    listing_id = int(path[9:])
+    print(listing_id)
+    newPath = ""
+
+    # if path[9:] != int:
+    # return "static/html/404.html"
+    
+    for dicts in listing:
+        if dicts.get("numeric ID") == listing_id:
+            newPath = (dicts["url"])
+            return newPath
+    
+    # return (f"{newPath}")
+    # return "/static/html/listing.html"
+    # return "static\html\listing_example.html"
+    
+    
+    
+
+    
 
 
 def render_gallery(query, category):
@@ -93,14 +116,20 @@ def server(url):
     The second is the content-type.
     """
     # YOUR CODE GOES HERE!
+    global path
     path = url.split("?")[0]
+    global listing_id
+    # print(path)
 
-    if path == "/" or path == "/main":
+    if path in "/" or path in "/main":
         return open("static/html/mainpage.html").read(), "text/html"
-    elif path == "/gallery":
+    elif "/gallery" in path:
         return open("static/html/listings.html").read(), "text/html"
-    elif path == "/listing/1":
-        return open("static/html/listing_example.html").read(), "text/html"
+    elif "/listing" in path:
+        newPath = render_listing(listings)
+        return open(newPath).read(), "text/html"
+        # return open(render_listing(listings)).read(), "text/html"
+        # print(render_listing(listings))
     else:
         return open("static/html/404.html").read(), "text/html"
 
@@ -141,3 +170,5 @@ def run():
 
 
 run()
+
+# render_listing(listings)
