294507d79f782d64b05e8797e862e91082e669bf
unknown <vang3827@umn.edu>
2024-09-30 20:10:48 -0500

    server.py done

--- server.py
+++ server.py
@@ -1,39 +1,87 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import re
+
+listings = [
+    {"vehicle 1":"Dodge Challenger","url":"https://images.unsplash.com/photo-1632686341369-8a7991237930?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D","description":"Longer text for descrption", "category":"car", "numeric ID": "1", "Date": "08/20/2024","bids":["$4,500","$5,000","$7,200"]},
+    {"vehicle 2":"Ford Mustang","url":"https://images.unsplash.com/photo-1625231334168-35067f8853ed?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D","description":"Longer text for descrption", "category":"car", "numeric ID": "2", "Date": "09/15/2024","bids":["$2,500","$5,500","$9,200"]},
+    {"vehicle 3":"Toyota Tundra","url":"https://images.unsplash.com/photo-1621993202323-f438eec934ff?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D","description":"Longer text for descrption", "category":"truck", "numeric ID": "3", "Date": "09/10/2024","bids":["$8,500","$9,000"]}
+]
+
+# PUT YOUR GLOBAL VARIABLES AND HELPER FUNCTIONS HERE.
+
+
+def escape_html(str):
+    str = str.replace("&", "&amp;")
+    str = str.replace('"', "&quot;")
+
+    # you need more.
+
+    return str
+
+
+def unescape_url(url_str):
+    import urllib.parse
+
+    # NOTE -- this is the only place urllib is allowed on this assignment.
+    return urllib.parse.unquote_plus(url_str)
+
+
+def parse_query_parameters(response):
+    # Split the query string into key-value pairs
+
+    # Initialize a dictionary to store parsed parameters
+
+    # Iterate over each key-value pair
+    # Split the pair by '=' to separate key and value
+
+    return {}
+
+
+def render_listing(listing):
+    pass
+
+
+def render_gallery(query, category):
+    pass
+
+
+# Provided function -- converts numbers like 42 or 7.347 to "$42.00" or "$7.35"
+def typeset_dollars(number):
+    return f"${number:.2f}"
+
 
 def server(url):
     """
-    url is a *PARTIAL* URL. If the browser requests `http://localhost:4131/contact?name=joe`
-    then the `url` parameter will have the value "/contact?name=joe". (so the schema and 
-    authority will not be included, but the full path, any query, and any anchor will be included)
+    url is a *PARTIAL* URL. If the browser requests `http://localhost:4131/contact?name=joe#test`
+    then the `url` parameter will have the value "/contact?name=joe". So you can expect the PATH
+    and any PARAMETERS from the url, but nothing else.
 
     This function is called each time another program/computer makes a request to this website.
     The URL represents the requested file.
 
-    This function should return a string.
+    This function should return two strings in a list or tuple. The first is the content to return
+    The second is the content-type.
     """
-
-    ######
-    # TODO: Hey student! This is the function you need to change! Don't miss it!
-    ######
+    # YOUR CODE GOES HERE!
     path = url.split("?")[0]
 
     if path == "/" or path == "/main":
-        return open("static/html/mainpage.html").read()
+        return open("static/html/mainpage.html").read(), "text/html"
     elif path == "/gallery":
-        return open("static/html/listings.html").read()
+        return open("static/html/listings.html").read(), "text/html"
     elif path == "/listing/1":
-        return open("static/html/listing_example.html").read()
+        return open("static/html/listing_example.html").read(), "text/html"
     else:
-        return open("static/html/404.html").read()
-    
+        return open("static/html/404.html").read(), "text/html"
 
 # You shouldn't need to change content below this. It would be best if you just left it alone.
+
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         # Call the student-edited server code.
-        message = server(self.path)
-        
+        message, content_type = server(self.path)
+
         # Convert the return value into a byte string for network transmission
         if type(message) == str:
             message = bytes(message, "utf8")
@@ -45,17 +93,21 @@ class RequestHandler(BaseHTTPRequestHandler):
         # Send headers
         # Note -- this would be binary length, not string length
         self.send_header("Content-Length", len(message))
-        self.send_header("Content-Type", "text/html; charset=utf-8")
+        self.send_header("Content-Type", content_type)
+        self.send_header("X-Content-Type-Options", "nosniff")
         self.end_headers()
 
         # Send the file.
         self.wfile.write(message)
         return
 
+
 def run():
     PORT = 4131
     print(f"Starting server http://localhost:{PORT}/")
-    server = ('', PORT)
+    server = ("", PORT)
     httpd = HTTPServer(server, RequestHandler)
     httpd.serve_forever()
+
+
 run()
