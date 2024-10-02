44b5e7e12eb1d4869ef86086812b48996f11713a
unknown <vang3827@umn.edu>
2024-10-01 21:05:23 -0500

    starting css

--- server.py
+++ server.py
@@ -2,26 +2,49 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import re
 
 listings = [
-    {"vehicle 1":"Dodge Challenger","url":"static/html/listing1.html","description":"Longer text for descrption", "category":"coupe", "numeric ID": 1, "Date": "08/20/2024",
+    {"vehicle":"Dodge Challenger","url":"static/html/listing1.html","description":"Longer text for descrption", "category":"small", "numeric ID": 1, "Date": "08/20/2024",
      "bids":[
          {"bidder 1":"Carl k", "bid amount":"4,500", "comment":"I would love this car."},{"bidder 2":"Sam Samson", "bid amount":"5,500", "comment":"I just want to use money."}
          ]
     },
-    {"vehicle 2":"Ford Mustang","url":"static/html/listing2.html","description":"Longer text for descrption", "category":"coupe", "numeric ID": 2, "Date": "09/15/2024",
+    {"vehicle":"Ford Mustang","url":"static/html/listing2.html","description":"Longer text for descrption", "category":"small", "numeric ID": 2, "Date": "09/15/2024",
      "bids":[
          {"bidder 1":"Luke Lukenson", "bid amount":"2,500", "comment":"Dailer driver right here."},{"bidder 2":"Peter Porker", "bid amount":"6,500", "comment":""}
          ]},
-    {"vehicle 3":"Toyota Tundra","url":"static/html/listing3.html","description":"Longer text for descrption", "category":"truck", "numeric ID": 3, "Date": "09/10/2024",
+    {"vehicle":"Toyota Tundra","url":"static/html/listing3.html","description":"Longer text for descrption", "category":"truck", "numeric ID": 3, "Date": "09/10/2024",
      "bids":[
          {"bidder 1":"Adam Apple", "bid amount":"6,500", "comment":"Work truck."},{"bidder 2":"Ben Benji", "bid amount":"6,500", "comment":"New truck for me."},{"bidder 3":"Cedar Cider", "bid amount":"9,500", "comment":"The Cedar Mobile."}
          ]},
-    {"vehicle 4":"Subaru Forester","url":"static/html/listing4.html","description":"Longer text for descrption", "category":"suv", "numeric ID": 4, "Date": "09/10/2024",
+    {"vehicle":"Toyota RAV4","url":"static/html/listing4.html","description":"Longer text for descrption", "category":"suv", "numeric ID": 4, "Date": "09/10/2024",
      "bids":[
          {"bidder 1":"Mike Micheal", "bid amount":"3,500", "comment":""},{"bidder 2":"Justin Time", "bid amount":"4,500", "comment":"Family vehicle."}
          ]}
 ]
 
 # PUT YOUR GLOBAL VARIABLES AND HELPER FUNCTIONS HERE.
+global path
+global listing_id
+global query
+global vehicleList
+
+def checkListDict (query,category):
+    print("in ChecklistDict")
+    
+    vehicleList=[]
+    # for dicts in listings:
+    #         # print(dicts)
+    #         if (query.lower() in dicts.get("vehicle").lower()):
+    #             vehicleList.append(dicts["vehicle"])
+                # print("in checkListDict function")
+    for i in range(len(listings)):
+        # print(listings[i])
+        # if (query.lower() in (i.get("vehicle").lower)):
+        #     vehicleList.append(i)
+    # return vehicleList
+        if(query.lower() in listings[i].get("vehicle").lower()):
+            vehicleList.append(listings[i])
+    print(vehicleList)
+    return vehicleList
 
 def escape_html(str):
     str = str.replace("&", "&amp;")
@@ -62,14 +85,14 @@ def parse_query_parameters(response):
             newStrList.append((newKV[j], newKV[j+1]))
         
     urlDict=dict(newStrList)
-
-    return {urlDict}
+    print(urlDict)
+    return urlDict
 
 
 def render_listing(listing):
-    print("in render listing")
+    # print("in render listing")
     listing_id = path[9:]
-    print(listing_id)
+    # print(listing_id)
     newPath = ""
     
     try:
@@ -85,7 +108,67 @@ def render_listing(listing):
 
 
 def render_gallery(query, category):
-    pass
+    print(type(query),query,type(category),category)
+    
+    galleryList = checkListDict(query,category)
+    # name = gallList.get("vehicle")
+
+    listingname = []
+    numOfBids = []
+    cat = []
+    date = []
+
+    for i in range(len(galleryList)):
+        listingname.append(galleryList[i].get("vehicle"))
+        cat.append(galleryList[i].get("category"))
+        numOfBids.append(galleryList[i].get("bids"))
+        date.append(galleryList[i].get("Date"))
+
+
+
+
+    return f"""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Auto Auction</title>
+    </head>
+    <body>
+        <form action="gallery" method="get" class="topnav">
+            <a href="/">About</a>
+            <a href="/gallery">Gallery</a>
+            <input name ="query" type="search" placeholder="Search..">
+            <select name="category" id="cars">
+                <option>None</option>
+                <option value="coupe">Coupe</option>
+                <option value="truck">Truck</option>
+                <option value="suv">SUV</option>
+            </select>
+            <input type="submit" value="Submit">
+        </form>
+        <h1>Query:{query} and Catagory:{category} </h1>
+        <p>{listingname}</p>
+        <table>
+            <table border="1">
+        <tr>
+            <th>Listing</th>
+            <th>Number of Bids</th>
+            <th>Top Bid</th>
+            <th>Auction Ends</th>
+        </tr>
+        <tr>
+            <td><a href = "/listing/1">{listingname}</a></td>
+            <td>{numOfBids}</td>
+            <td>{cat}</td>
+            <td>{date}</tb>
+        </tr>
+    </table>
+        </table>
+    </body>
+    </html>
+    ""","text/html"
 
 
 # Provided function -- converts numbers like 42 or 7.347 to "$42.00" or "$7.35"
@@ -106,20 +189,29 @@ def server(url):
     The second is the content-type.
     """
     # YOUR CODE GOES HERE!
-    global path
+    # global path
+    # global listing_id
+    # global query
     path = url.split("?")[0]
-    global listing_id
+    
+    
     # print(path)
 
     if path in "/" or path in "/main":
         return open("static/html/mainpage.html").read(), "text/html"
     elif "/gallery" in path:
-        return open("static/html/listings.html").read(), "text/html"
+        if "?" in url:
+            print(url)
+            query = url.split("?")[1]
+            print(query)
+            return render_gallery(parse_query_parameters(query).get("query"),parse_query_parameters(query).get("category"))
+        else:
+            return open("static/html/listings.html").read(), "text/html"
     elif "/listing" in path:
         newPath = render_listing(listings)
         return open(newPath).read(), "text/html"
-        # return open(render_listing(listings)).read(), "text/html"
-        # print(render_listing(listings))
+    elif "/main.css":
+        return open("static/css/main.css").read(), "text/css"
     else:
         return open("static/html/404.html").read(), "text/html"
 
@@ -162,3 +254,4 @@ def run():
 run()
 
 # render_listing(listings)
+# print(parse_query_parameters("?query=tundra&category=truck"))
